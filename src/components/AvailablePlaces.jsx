1178d336524a86da8d9251b36af8478f09bfabe6
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-03-27 14:00:47 +0100

    loading state and error catching added

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,18 +1,54 @@
 import { useEffect, useState } from 'react';
 
 import Places from './Places.jsx';
+import ErrorPage from './Error.jsx'
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
 
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
-  useEffect(() => {
-    fetch('http://localhost:3000/places').then((response) => {
-      return response.json();
-    }).then((resData) => {
-      setAvailablePlaces(resData.places);
-    });
+  useEffect( () => {
+    setIsFetching(true);
+
+    const fetchPlaces = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/places`);
+        const resData = await response.json();
+        if(!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+              resData.places,
+              position.coords.latitude,
+              position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message: error.message || 'Could not fetch places, please try again later.',
+        });
+        setIsFetching(false);
+      }
+    }
+
+    fetchPlaces();
+
   }, [])
+
+  if(error) {
+    return <ErrorPage
+      title="An error occurred!"
+      message={error.message}
+    />;
+  }
+
 
   return (
     <Places
@@ -20,6 +56,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
       places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
     />
   );
 }
